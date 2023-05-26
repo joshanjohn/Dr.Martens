783c1a94f3b8e2c0341f664de4347c69b2a49522
Puvatida Simcharoen <puvatida.simcharoen@student.griffith.ie>
2023-05-26 13:28:17 +0100

    created 2 draft sets of gallery page with lightbox

--- script2.0.js
+++ script2.0.js
@@ -1,10 +1,13 @@
 window.onload  = function() {
 
-
-
-
+    lightbox.option({ 
+        resizeDuration: 200,
+        wrapAround: true,
+        disableScrolling: true,
+        fitImagesInViewport: true,
+        imageFadeDuration: 60,
+      })
+// resize images so view dont need to scroll so view images. 
+// imagefaded, fades once image is open 
 
 } //window.onload
-
-// https://lokeshdhakar.com/projects/lightbox2/ 
-// lightbox info downloads etc
